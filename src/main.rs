5acb3a062d8c4d756e308ae4808bab10b0e61527
wilzh40 <wilzh40@gmail.com>
2015-06-03 19:02:48 -0700

    Created bodies vector and its iterator

--- src/main.rs
+++ src/main.rs
@@ -36,13 +36,8 @@ impl Body {
 
            color: (1.0, 1.0, 1.0, 1.0),
 
-
        }
-
-
     }
-
-
 
 
 }
@@ -63,23 +58,32 @@ impl App {
         const GREEN: [f32; 4] = [0.0, 1.0, 0.0, 1.0];
         const RED: [f32; 4] = [1.0, 0.0, 0.0, 1.0];
         const BLUE: [f32; 4] = [0.0, 0.0, 1.0, 1.0];
-        const BLACK: [f32; 4] = [1.0, 1.0, 1.0, 1.0];
-        const WHITE: [f32; 4] = [0.0, 0.0, 0.0, 1.0];
+        const WHITE: [f32; 4] = [1.0, 1.0, 1.0, 1.0];
+        const BLACK: [f32; 4] = [0.0, 0.0, 0.0, 1.0];
 
         // Create the square
         let square = rectangle::square(0.0, 0.0, 50.0);
+        let circle = rectangle::square(0.0, 0.0, 20.0);
         let rotation = self.rotation;
         let body_count = self.body_count;
         let (x, y) = ((args.width/2) as f64, (args.height/2) as f64);
+        for b in &self.bodies {
+            self.gl.draw(args.viewport(), |c, gl| {
+                clear(BLACK, gl);
 
-        self.gl.draw(args.viewport(), |c, gl| {
-            clear(BLACK, gl);
+                let transform = c.transform.trans(x, y).rot_rad(rotation).trans(-25.0,-25.0);
+                
+                rectangle(WHITE, square, transform, gl);
 
-            let transform = c.transform.trans(x, y).rot_rad(rotation).trans(-25.0,-25.0);
-
-            rectangle(WHITE, square, transform, gl);
-        });
+               // for b in self.bodies {
+    //                let circle = rectangle::square(b.
+                 //   ellipse(WHITE, circle, c.transform, gl); 
+             //   }
+            });
+        }
+       
      }
+    
     fn update(&mut self, args: &UpdateArgs){
 
         // Insert gravity code here
@@ -90,21 +94,25 @@ impl App {
     fn handleInput (&mut self, i:Input ) {
         
         match i {
-            Input::Release(Mouse(Left)) => { 
+            Input::Release(Mouse(left)) => { 
                 println!("Button Released");
-                if let Some(args) = i.mouse_cursor_args(){
-                }
-             
-                 println!("{:?}",i.mouse_cursor_args());
-            
+                println!("{:?}",i.mouse_cursor_args());
+                
+
+                println!("{:?}", self.current_cursor_position);
+                
+                // Adds a new body to the stage
+
+                let b: Body = Body::new(self.current_cursor_position, 10.0, 10.0);
+                self.bodies.push(b);
+
             },
 
             Input::Move(MouseCursor(x, y)) => {
-                println!("({},{})", x, y);
+                self.current_cursor_position = (x, y);
             }
 
-
-            _=> { println!("input");}
+            _=> {}
             
 
 
